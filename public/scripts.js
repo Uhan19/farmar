c9b204f595d923b4a6f6a2d9864981d3d8147a3c
Samuel He <samuelhe83@gmail.com>
2017-08-06 21:55:48 -0700

    Work on ProductDetailPage

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const images = document.querySelectorAll('.ItemImage');
+
+function hasSelected() {
+  for (var i = 0; i < images.length; i++) {
+    if (images.classList.contains('selected')) {
+      return true;
+    }
+  }
+  return false;
+}
+
+if (!hasSelected) {
+  images[0].classList.add('selected');
+}
+
+function handleImageClick() {
+  for (var i = 0; i < images.length; i++) {
+    if (images[i].classList.contains('selected')) {
+      images[i].classList.remove('selected');
+    }
+  }
+  this.classList.add('selected');
+}
+
+image.forEach(function(ele) {
+  ele.addEventListener('click', handleImageClick);
+});
