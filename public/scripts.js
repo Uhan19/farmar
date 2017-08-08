b026f057aff9c79df2ccc7a4cb9744a552f0329e
Samuel He <samuelhe83@gmail.com>
2017-08-07 17:28:12 -0700

    Add click functionality to product detail page

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,22 @@
 const images = document.querySelectorAll('.ItemImage');
+const SelectedImage = document.querySelector('.SelectedImage');
+const ImageLink = document.querySelector('.ItemImageLink');
+const Name = document.querySelector('.SelectedImageName');
 
 function hasSelected() {
   for (var i = 0; i < images.length; i++) {
-    if (images.classList.contains('selected')) {
+    if (images[0].classList.contains('selected')) {
       return true;
     }
   }
   return false;
 }
 
-if (!hasSelected) {
+if (!hasSelected()) {
   images[0].classList.add('selected');
 }
+
+const SelectedThumbnail = document.querySelector('.selected');
 
 function handleImageClick() {
   for (var i = 0; i < images.length; i++) {
@@ -20,8 +25,17 @@ function handleImageClick() {
     }
   }
   this.classList.add('selected');
+  SelectedImage.src = this.src;
+  ImageLink.href = this.src;
+  SelectedImage.alt = this.name;
+  Name.innerHTML = this.name;
 }
 
-image.forEach(function(ele) {
+images.forEach(function(ele) {
   ele.addEventListener('click', handleImageClick);
 });
+
+SelectedImage.src = SelectedThumbnail.src;
+SelectedImage.alt = SelectedThumbnail.name;
+ImageLink.href = SelectedThumbnail.src;
+Name.innerHTML = SelectedThumbnail.name;
