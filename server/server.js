007164bbd4409f854cd686e771010b20bac0bc66
Chan Kwon <chanthemancan@gmail.com>
2017-08-08 21:30:31 -0700

    Invoice page styled and connected. Email server fully functional. InvoiceSent page needs picture and text

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const server = email.server.connect({
 });
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static(path.join(__dirname, '../assets')));
 app.use(express.static(path.join(__dirname, '../public/Home.css')));
@@ -59,23 +60,28 @@ app.get('/Invoice', (req, res) => {
 });
 
 app.post('/Invoice', (req, res) => {
-  console.log('req', req.params);
+  console.log('req', req.body);
+  const email = {
+    from: req.body.FromEmail,
+    body: req.body.EmailBody,
+    cc: req.body.CCOption ? req.body.FromEmail : ''
+  };
   res.sendFile(path.join(__dirname + '/../public/Invoice.html'));
-  // server.send(
-  //   {
-  //     text: ,
-  //     from: `GTRagsupplies <${env.GMAIL_USERNAME}@gmail.com>`,
-  //     to: `GTRagsupplies <${env.GMAIL_RECEIVER}@gmail.com>`,
-  //     cc: 'else <hschan11@aim.com',
-  //     subject: `Invoice request from ${}`
-  //   },
-  //   (err, message) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //   }
-  // );
-  // res.redirect('/InvoiceSent');
+  server.send(
+    {
+      text: 'from \n' + email.from + '\n\n\n' + email.body,
+      from: `GTRagsupplies <${env.GMAIL_USERNAME}>`,
+      to: `GTRagsupplies <${env.GMAIL_RECEIVER}>`,
+      cc: 'else <hschan11@aim.com',
+      subject: `Invoice request from ${email.body}`
+    },
+    (err, message) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  res.redirect('/InvoiceSent');
 });
 
 app.get('/InvoiceSent', (req, res) => {
