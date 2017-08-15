b667b0c6019282cc4f7b41a10e831c7a81476c79
Chan Kwon <chanthemancan@gmail.com>
2017-08-14 22:41:24 -0700

    Update insect mesh picture

--- server/productData.js
+++ server/productData.js
@@ -8,88 +8,88 @@ module.exports = {
       'https://dl.dropboxusercontent.com/s/9xdgk3i9muya4hh/Hoops%20%283%29.jpg',
       'https://dl.dropboxusercontent.com/s/thwajlfqjpaotay/Hoops%20%285%29.jpg',
       'https://dl.dropboxusercontent.com/s/gaafld83zgvasx2/Post.jpg',
-      'https://dl.dropboxusercontent.com/s/smdnxcx3qhj9ha3/Post%20%282%29.jpg',
-    ],
+      'https://dl.dropboxusercontent.com/s/smdnxcx3qhj9ha3/Post%20%282%29.jpg'
+    ]
   },
   'Greenhouse Plastic': {
     images: [
       'https://dl.dropboxusercontent.com/s/0wc85foqgqx6dfp/Plastic.jpeg',
       'https://dl.dropboxusercontent.com/s/895o066rj91a6ce/Plastic%20%282%29.jpg',
       'https://dl.dropboxusercontent.com/s/vrbmuvpspslozdv/Plastic%20%283%29.jpg',
-      'https://dl.dropboxusercontent.com/s/sanzohkvk78q0i9/Plastic%20%284%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/sanzohkvk78q0i9/Plastic%20%284%29.JPG'
+    ]
   },
   'Ground Cover': {
     images: [
       'https://dl.dropboxusercontent.com/s/qymymgvsz15l2em/Ground%20Cover.jpg',
       'https://dl.dropboxusercontent.com/s/v0xfgrmwyjk9ju8/Ground%20Cover%20%282%29.jpg',
       'https://dl.dropboxusercontent.com/s/sqhvy9fz59vf19u/Ground%20Cover%20%283%29.jpg',
-      'https://dl.dropboxusercontent.com/s/0f5kzk4nmxz2vxa/Ground%20Cover%20%284%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/0f5kzk4nmxz2vxa/Ground%20Cover%20%284%29.JPG'
+    ]
   },
   'Mini Tunnel Strawberries': {
     images: [
       'https://dl.dropboxusercontent.com/s/wjm9fa64l29zzb3/Mini%20Tunnel.JPG',
       'https://dl.dropboxusercontent.com/s/untkqzlvr2hyugr/Mini%20Tunnel%20%282%29.JPG',
-      'https://dl.dropboxusercontent.com/s/hpx1eebehiv3x3j/Mini%20Tunnel%20%283%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/hpx1eebehiv3x3j/Mini%20Tunnel%20%283%29.JPG'
+    ]
   },
   Mulch: {
     images: [
       'https://dl.dropboxusercontent.com/s/zvsta1poexg3y9j/Mulch.jpg',
       'https://dl.dropboxusercontent.com/s/ycj5ggf5o1dk98d/Mulch%20%282%29.jpg',
-      'https://dl.dropboxusercontent.com/s/tlibx3o7qrpotpw/Mulch%20%283%29.jpg',
-    ],
+      'https://dl.dropboxusercontent.com/s/tlibx3o7qrpotpw/Mulch%20%283%29.jpg'
+    ]
   },
   Rope: {
     images: [
       'https://dl.dropboxusercontent.com/s/ti9k9dgdz82ef0p/Rope.jpg',
-      'https://dl.dropboxusercontent.com/s/lguta9r4jnzjxrs/Rope%20%282%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/lguta9r4jnzjxrs/Rope%20%282%29.JPG'
+    ]
   },
   Trellis: {
     images: [
       'https://dl.dropboxusercontent.com/s/62ybxkcsiwaby41/Trellis.jpg',
       'https://dl.dropboxusercontent.com/s/t3x0rxv58m5ca56/Trellis%20%282%29.JPG',
       'https://dl.dropboxusercontent.com/s/t3x0rxv58m5ca56/Trellis%20%282%29.JPG',
-      'https://dl.dropboxusercontent.com/s/27bn7f7q7fojknr/Trellis%20%284%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/27bn7f7q7fojknr/Trellis%20%284%29.JPG'
+    ]
   },
   Twine: {
     images: [
       'https://dl.dropboxusercontent.com/s/2ukfl04xcfrp2wo/Twine.JPG',
       'https://dl.dropboxusercontent.com/s/2ukfl04xcfrp2wo/Twine.JPG',
-      'https://dl.dropboxusercontent.com/s/6l9c1fsxjlfchat/Twine%20%283%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/6l9c1fsxjlfchat/Twine%20%283%29.JPG'
+    ]
   },
   Valves: {
     images: [
       'https://dl.dropboxusercontent.com/s/8kpkvu21heaza0l/Automation.JPG',
       'https://dl.dropboxusercontent.com/s/xc7qiaifo5500qf/Automation%20%282%29.JPG',
       'https://dl.dropboxusercontent.com/s/mo4u6zdwx1ktswn/Valve.jpg',
-      'https://dl.dropboxusercontent.com/s/561wx959k33jgly/Valve%20%282%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/561wx959k33jgly/Valve%20%282%29.JPG'
+    ]
   },
   Windbreak: {
     images: [
       'https://dl.dropboxusercontent.com/s/pg8p04357vdlhh5/Windbreak.JPG',
       'https://dl.dropboxusercontent.com/s/6f1obau49kwjplj/Windbreak%20Posts.jpg',
-      'https://dl.dropboxusercontent.com/s/7ltjb18b5pa29vo/Windbreak%20Posts%20%282%29.JPG',
-    ],
+      'https://dl.dropboxusercontent.com/s/7ltjb18b5pa29vo/Windbreak%20Posts%20%282%29.JPG'
+    ]
   },
   Birdnet: {
-    images: ['https://dl.dropboxusercontent.com/s/y7h0q604817bjqu/Birdnet.jpg'],
+    images: ['https://dl.dropboxusercontent.com/s/y7h0q604817bjqu/Birdnet.jpg']
   },
   'Frozen Cloth': {
-    images: ['https://dl.dropboxusercontent.com/s/sm3joslmtbxzk7h/Frozen%20Cloth.jpg'],
+    images: ['https://dl.dropboxusercontent.com/s/sm3joslmtbxzk7h/Frozen%20Cloth.jpg']
   },
   'Insect Mesh': {
-    images: ['https://dl.dropboxusercontent.com/s/sm3joslmtbxzk7h/Frozen%20Cloth.jpg'],
+    images: ['https://dl.dropboxusercontent.com/s/p4c6l0jwnk2w2z5/Insect%20Mesh.jpg']
   },
   'Shade Cloth': {
-    images: ['https://dl.dropboxusercontent.com/s/18a98ck657nwyhw/Shade%20Cloth.jpg'],
+    images: ['https://dl.dropboxusercontent.com/s/18a98ck657nwyhw/Shade%20Cloth.jpg']
   },
   'Wires 12ga, 14ga, 16ga': {
-    images: ['https://dl.dropboxusercontent.com/s/v4g0tzl0q98j1b0/Wires%2012ga%2C%2014ga%2C%2016ga.JPG'],
-  },
+    images: ['https://dl.dropboxusercontent.com/s/v4g0tzl0q98j1b0/Wires%2012ga%2C%2014ga%2C%2016ga.JPG']
+  }
 };
