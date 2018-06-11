cf129bf8edfd9035c145c7bc07ccd8df827f0f0b
Chan Kwon <chanthemancan@gmail.com>
2018-06-10 17:56:41 -0700

    delete .env.js

--- .env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  GMAIL_USERNAME: 'GrandTophamRanch',
-  GMAIL_PASSWORD: 'TelAviv2020',
-  GMAIL_RECEIVER: 'shlomo.amar@gtragsupplies.com'
-  GMAIL_RECEIVER2: 'Yoram.kesten@gtragsupplies.com'
-};
